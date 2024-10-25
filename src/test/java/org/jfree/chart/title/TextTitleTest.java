c5f51cbe110a5399a5f8d7f8b3e1a926e7237339
Joakim Colloz <kung_jocke@hotmail.com>
2024-10-25 09:50:17 +0200

    Remove star import in TextTitleTest

--- src/test/java/org/jfree/chart/title/TextTitleTest.java
+++ src/test/java/org/jfree/chart/title/TextTitleTest.java
@@ -55,7 +55,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests for the {@link TextTitle} class.
